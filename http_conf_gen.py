0a04c8940105f27e431ed793cbacd3834850e7e1
FUNk31r0 <cardoso.natanael2012@gmail.com>
2017-09-06 14:28:21 -0300

    Create http_conf_gen.py

---
+++ http_conf_gen.py
@@ -0,0 +1,13 @@
+import sys
+arq = open(sys.argv[1], 'r')
+
+for line in arq:
+        line = line.replace("\n", "")
+        line = line.replace("\r", "")
+        print("""
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/{}"
+    ServerName {}
+    Redirect / https://{}
+</VirtualHost>
+""".format(line, line, line))
